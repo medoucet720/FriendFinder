2a75cb35cdbac843fba4350cdbd14b72181096a2
Mary Doucet <marydoucet@Marys-MacBook-Pro.local>
2018-03-20 21:36:43 -0400

    heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendList = [
   {
     name: 'Mary Doucet',
-    profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+    profilePic: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fbluewatermtg.com%2Fwp-content%2Fuploads%2F2013%2F12%2FIMG_4911.jpg&imgrefurl=https%3A%2F%2Fbluewatermtg.com%2Four-team%2F&docid=LgP-GEdCqb8_5M&tbnid=wyw1nsK7hiSzLM%3A&vet=10ahUKEwjh6fz9m_zZAhWl24MKHe_aDWMQMwiFASg-MD4..i&w=142&h=168&bih=760&biw=1311&q=mary%20doucet%20photo&ved=0ahUKEwjh6fz9m_zZAhWl24MKHe_aDWMQMwiFASg-MD4&iact=mrc&uact=8',
     scores: [
       "4",
       "5",
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,9 +4,15 @@ var path = require('path');
 
 module.exports = function(app){
   //default GET route that leads to home.html - displays home page
+
+  app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../public/home.html"));
+});
   app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    res.sendFile(path.join(__dirname , '/../public/survey.html'));
   });
+
+
 
   //a USE route to home page
   app.use(function (req, res) {
